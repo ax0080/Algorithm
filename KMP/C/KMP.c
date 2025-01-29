9d213aed4dc75677103137490291d0f59176b5c5
Min-Yan Hsieh <myhsieh2000@gmail.com>
2025-01-30 02:15:18 +0800

    index finish

--- KMP/C/KMP.c
+++ KMP/C/KMP.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
 #include <string.h>
-/*
-char *index(char *pattern)
-{
+#include <stdlib.h>
 
+int *index_map(char *pattern)
+{
+    int pattern_length = strlen(pattern);
+    int *index = (int *) malloc(sizeof(int) * pattern_length);
+    
+    
+    int match = 0;
+    index[0] = match;
+    int flag; // compared index from the front
+    int i = 1; // current index from the back
+    while(i < pattern_length)
+    {
+        flag = index[i-1];
+        // this while loop is the most important and difficult in the algorithm
+        while((pattern[flag] != pattern[i]) && (flag != 0))
+        {
+            flag = index[flag - 1];
+        }
+        
+        if(pattern[flag] == pattern[i]){
+            match = flag + 1;
+        }
+        else match = 0;
+
+        index[i] = match;
+        i++;
+    }
+
+    return index;
 }
-*/
+
 
 void KMP(char *text, char *pattern)
 {
@@ -14,7 +41,7 @@ void KMP(char *text, char *pattern)
 
 
     //==================== solution 1 : O(n) = n^2 ======================
-    
+    /*
     for(int i=0; i<text_length; i++)
     {
         int j = 0;
@@ -30,15 +57,20 @@ void KMP(char *text, char *pattern)
             printf("find pattern %s in test %d position\n", pattern, i);
         }
     }
-    
+    */
     
     //==================== solution 2 : KMP search ======================
-    /*
-    char *index = malloc(sizeof(char) * pattern_length); 
+    int *index = (int *) malloc(sizeof(int) * pattern_length); 
+    index = index_map(pattern);
     
+    for(int i=0; i<pattern_length; i++)
+    {
+        printf("%d ", *(index+i));
+    }
 
-    free()
-    */
+
+    free(index);
+    return;
 }
 
 int main(){
