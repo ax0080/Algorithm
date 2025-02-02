202e384f36175e3e1212b68834bd9e620c22431c
Min-Yan Hsieh <myhsieh2000@gmail.com>
2025-02-03 00:08:38 +0800

    mazing stack.h complete

--- Mazing/C/Stack.h
+++ Mazing/C/Stack.h
@@ -15,17 +15,27 @@ typedef struct {
 typedef struct {
     Node *node;
     int top;
-    
-
+    int capacity;
 } NodeStack;
 
 typedef struct {
     NodeStack nodestack;
 
     void (*init)(NodeStack* ,int );
-    bool (*isEmpty)(NodeStack*)
-
-
+    bool (*isEmpty)(NodeStack*);
+    void (*push)(NodeStack* ,int );
+    int (*pop)(NodeStack* );
+    int (*peek)(NodeStack* );
+    void (*expand)(NodeStack* );
+    void (*free)(NodeStack* );
 } NodeWrapper;
+
+void init(NodeStack *nodestack, int initial_capacity);
+bool isEmpty(NodeStack *nodestack);
+void push(NodeStack *nodestack);
+int pop(NodeStack *nodestack);
+int peek(NodeStack *nodestack);
+void expand(NodeStack *nodestack);
+void free(NodeStack *nodestack);
 
 #endif
